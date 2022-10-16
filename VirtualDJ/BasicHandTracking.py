43ee44ee66472b4858142d8309535bcee5995770
O'Neal Kpodar <onealkpodar@ONeals-MacBook-Air.local>
2022-10-16 01:02:20 -0400

    Upload

--- VirtualDJ/BasicHandTracking.py
+++ VirtualDJ/BasicHandTracking.py
@@ -1,105 +1,112 @@
 import cv2
 import mediapipe as mp
 import time
+
+import pydub.effects
+
 import Buttons as btns
-import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 
 import librosa
 import librosa.display
-from IPython.display import Audio
 from pydub import AudioSegment
-from pydub.playback import play
 
 
 song1 = AudioSegment.from_file("test.wav", format="wav")
-song2 = AudioSegment.from_file("BussDown.mp3", format="mp3")
-play(song1)
-# play(song2)
-
+song2 = AudioSegment.from_file("BussDown.mp3", format="mp3") - 30
 
 #Plotting the wavelengths from the song
-x_1, fs = librosa.load('test.wav')
+# x_1, fs = librosa.load('test.wav')
 
-Audio(x_1, rate=fs);
+# fig, ax = plt.subplots()
+# librosa.display.waveshow(x_1, sr=fs, ax=ax)
+# plt.show()
 
-fig, ax = plt.subplots()
-librosa.display.waveshow(x_1, sr=fs, ax=ax)
-plt.show()
+wheel1 = []
+cv2.imread("blueTurnTable.png", wheel1)
 
-
-
-cap = cv2.VideoCapture(0)
-mphands = mp.solutions.hands
-hands = mphands.Hands(False, 2, 1, .5, .2)
-mpDraw = mp.solutions.drawing_utils
-pTime = 0
-cTime = 0
-success, img = cap.read()
-height, width, c = img.shape
+wheel2 = []
+cv2.imread("purpleTurnTable.png", wheel2)
 
 
-musicWheelLeft = btns.MusicWheel(0, 0, width // 4, height // 2)
-bpmLeft = btns.VerticalSlider(0, 100, width // 16, height // 2)
-volumeLeft = btns.VerticalSlider(0, 100, width // 2 - width // 16, height // 2)
-leftButtonOne = btns.Button(2 * width // 16, height - height // 6)
-leftButtonTwo = btns.Button(3 * width // 16, height - height // 6)
-leftButtonThree = btns.Button(2 * width // 16, height - height // 6 + width // 16)
-leftButtonFour = btns.Button(3 * width // 16, height - height // 6 + width // 16)
-leftCueButton = btns.Button(4 * width // 16, height - height // 6, "C")
-leftPauseButton = btns.Button(4 * width // 16, height - height // 6 + width // 16, "P")
+def game():
 
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, 20)
+    mphands = mp.solutions.hands
+    hands = mphands.Hands(False, 2, 1, .5, .2)
+    mpDraw = mp.solutions.drawing_utils
 
-musicWheelRight = btns.MusicWheel(0, 0, width - width // 4, height // 2)
-bpmRight = btns.VerticalSlider(0, 100, width - width // 16, height // 2)
-volumeRight = btns.VerticalSlider(0, 100, width // 2 + width // 16, height // 2)
-rightButtonOne = btns.Button(width - 2 * width // 16, height - height // 6)
-rightButtonTwo = btns.Button(width - 3 * width // 16, height - height // 6)
-rightButtonThree = btns.Button(width - 2 * width // 16, height - height // 6 + width // 16)
-rightButtonFour = btns.Button(width - 3 * width // 16, height - height // 6 + width // 16)
-rightCueButton = btns.Button(width - 4 * width // 16, height - height // 6, "C")
-rightPauseButton = btns.Button(width - 4 * width // 16, height - height // 6 + width // 16, "P")
-
-masterVolume = btns.HorizontalSlider(0, 100, width // 2, height - 2 * height // 16)
-
-buttons = [musicWheelLeft, bpmLeft, volumeLeft, leftButtonOne, leftButtonTwo,
-           leftButtonThree, leftButtonFour, leftCueButton, leftPauseButton, rightButtonOne, rightButtonTwo,
-           rightButtonThree, rightButtonFour, rightCueButton, rightPauseButton,
-           musicWheelRight, bpmRight, volumeRight, masterVolume]
-
-
-while True:
     success, img = cap.read()
-    img = cv2.flip(img, 1)
-    # img = cv2.flip(img, 0)
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-
-    overlay = img.copy()
     height, width, c = img.shape
 
-    for button in buttons:
-        button.draw(overlay)
-    alpha = 0.6
-    img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+    bpmLeft = btns.VerticalSlider(0, 100, width // 16, height // 2)
+    volumeLeft = btns.VerticalSlider(0, 100, width // 2 - width // 16, height // 2)
+    leftButtonOne = btns.Button(2 * width // 16, height - height // 6)
+    leftButtonTwo = btns.Button(3 * width // 16, height - height // 6)
+    leftButtonThree = btns.Button(2 * width // 16, height - height // 6 + width // 16)
+    leftButtonFour = btns.Button(3 * width // 16, height - height // 6 + width // 16)
+    leftCueButton = btns.Button(4 * width // 16, height - height // 6, "C")
+    leftPauseButton = btns.Button(4 * width // 16, height - height // 6 + width // 16, "P")
+    musicWheelLeft = btns.MusicWheel(song1,
+                                     0, 0, width // 4, height // 2,
+                                     [volumeLeft, leftButtonOne, leftButtonTwo,
+                                      leftButtonThree, leftButtonFour, leftCueButton, leftPauseButton], wheel1)
 
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
+    bpmRight = btns.VerticalSlider(0, 100, width - width // 16, height // 2)
+    volumeRight = btns.VerticalSlider(0, 100, width // 2 + width // 16, height // 2)
+    rightButtonOne = btns.Button(width - 2 * width // 16, height - height // 6)
+    rightButtonTwo = btns.Button(width - 3 * width // 16, height - height // 6)
+    rightButtonThree = btns.Button(width - 2 * width // 16, height - height // 6 + width // 16)
+    rightButtonFour = btns.Button(width - 3 * width // 16, height - height // 6 + width // 16)
+    rightCueButton = btns.Button(width - 4 * width // 16, height - height // 6, "C")
+    rightPauseButton = btns.Button(width - 4 * width // 16, height - height // 6 + width // 16, "P")
+    musicWheelRight = btns.MusicWheel(song2,
+                                      0, 0, width - width // 4, height // 2,
+                                      [volumeRight, rightButtonOne, rightButtonTwo,
+                                       rightButtonThree, rightButtonFour, rightCueButton, rightPauseButton], wheel2)
 
-            indexPointer = handLms.landmark[8]
-            cx, cy = int(width * indexPointer.x), int(height * indexPointer.y)
-            for button in buttons:
-                if button.beingTouched(cx, cy, img):
+    masterVolume = btns.HorizontalSlider(0, 100, width // 2, height - 2 * height // 16)
+
+    buttons = [musicWheelLeft, musicWheelRight]
+
+    pTime = time.time()
+    totalTime = 0
+    while True:
+        success, img = cap.read()
+        img = cv2.flip(img, 1)
+        img = cv2.flip(img, 0)
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+
+        overlay = img.copy()
+        height, width, c = img.shape
+
+        for button in buttons:
+            button.go(totalTime)
+            button.draw(overlay)
+
+        alpha = 0.6
+        img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+
+                indexPointer = handLms.landmark[8]
+                cx, cy = int(width * indexPointer.x), int(height * indexPointer.y)
+                for button in buttons:
                     button.handleTouch(cx, cy, img)
-            cv2.circle(img, (cx, cy), btns.radius, (0, 139, 0), cv2.FILLED)
-            # Can include mphands.HAND_CONNECTIONS to draw the lines
-            # mpDraw.draw_landmarks(img, handLms,)
+                cv2.circle(img, (cx, cy), btns.radius, (0, 139, 0), cv2.FILLED)
+                # Can include mphands.HAND_CONNECTIONS to draw the lines
+                # mpDraw.draw_landmarks(img, handLms,)
 
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
+        cTime = time.time()
+        totalTime = int((cTime - pTime) * 1000)
+        pTime = cTime
+        cv2.putText(img, str(int(totalTime)), (40, 40), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0))
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
 
-    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (139, 0, 0), 4)
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+
+if __name__ == '__main__':
+    game()
--- VirtualDJ/Buttons.py
+++ VirtualDJ/Buttons.py
@@ -1,10 +1,12 @@
-import math
-from math import atan2, hypot
-
+from multiprocessing import Process
+from threading import Thread
+import os
 import cv2
+import pydub
+from pydub import AudioSegment, playback
 
 global radius
-radius = 20
+radius = 30
 
 
 def collidesRect(pt1, pt2, x, y) -> bool:
@@ -24,7 +26,7 @@ def collidesRect(pt1, pt2, x, y) -> bool:
     if circleDistanceY <= height // 2:
         return True
 
-    cornerDistance_sq = ((circleDistanceX - width // 2) ** 2) + (circleDistanceY - height // 2) ** 2;
+    cornerDistance_sq = ((circleDistanceX - width // 2) ** 2) + (circleDistanceY - height // 2) ** 2
     return cornerDistance_sq <= (radius ^ 2)
 
 
@@ -34,6 +36,8 @@ class Button:
         self.x = x
         self.y = y
         self.on = False
+        self.isColliding = False
+        self.index = -1
 
     def draw(self, img):
         height, width, c = img.shape
@@ -51,37 +55,124 @@ class Button:
 
     def beingTouched(self, x, y, img):
         height, width, c = img.shape
-        if not self.on:
-            return collidesRect((self.x - width // 40, self.y - width // 40),
-                                (self.x + width // 40, self.y + width // 40),
-                                x, y)
-        return False
+        return collidesRect((self.x - width // 40, self.y - width // 40),
+                            (self.x + width // 40, self.y + width // 40),
+                            x, y)
 
     def handleTouch(self, x, y, img):
-        self.on = True
+        self.on = not self.on
+        self.isColliding = True
 
 
 class MusicWheel:
-    def __init__(self, currentBeat: int, endBeat: int, x: int, y: int):
+    def __init__(self, song: AudioSegment, currentBeat: int, endBeat: int, x: int, y: int, buttonsToCheck, img):
+        self.ogSong = song
+        self.song = song
         self.currentBeat = currentBeat
         self.endBeat = endBeat
         self.x = x
         self.y = y
+        self.cues = []
+        self.time = 0
+        self.buttonsToCheck = buttonsToCheck
+        self.thread = playback._play_with_simpleaudio(self.song)
+        self.paused = False
+        self.img = img
+
+    def go(self, newTimeDiff):
+        if self.paused:
+            self.thread.stop()
+        else:
+            self.time += newTimeDiff
 
     def draw(self, img):
         height, width, c = img.shape
+        cv2.resize(self.img, width // 7, width //7)
         cv2.circle(img, (self.x, self.y), width // 7, (40, 40, 40), cv2.FILLED)
+        for button in self.buttonsToCheck:
+            button.draw(img)
 
     def beingTouched(self, x, y, img):
-        height, width, c = img.shape
-        # Considered Touched When on the outer 2/3rds of the wheel
-        return radius + width // 7 > hypot(self.x - x, self.y - y) > radius + ((width // 7) // 3)
+        for button in self.buttonsToCheck:
+            if button.beingTouched(x, y, img):
+                return True
+        return False
 
     def handleTouch(self, x, y, img):
-        angle = -atan2(y - self.y, x - self.x)
-        if angle < 0:
-            angle = atan2(y - self.y, self.x - x) + math.pi
-        # print((int(angle * 180 / math.pi) - 90) % 360 // 90)
+        for button in self.buttonsToCheck[0: 4]:
+            if not button.beingTouched(x, y, img):
+                button.isColliding = False
+
+        if self.buttonsToCheck[0].beingTouched(x, y, img):
+            self.buttonsToCheck[0].handleTouch(x, y, img)
+            self.thread.stop()
+            self.thread = playback._play_with_simpleaudio(self.song[self.time:]
+                                                          + (50 - self.buttonsToCheck[0].current) / 50 * 30)
+
+        if self.buttonsToCheck[1].beingTouched(x, y, img):
+            if not self.buttonsToCheck[1].isColliding:
+                if self.buttonsToCheck[1].on:
+                    print(f"Test: {self.time}")
+                    self.thread.stop()
+                    self.thread = playback._play_with_simpleaudio(self.song[self.buttonsToCheck[1].index:]
+                                                                  + (50 - self.buttonsToCheck[0].current) / 50 * 30)
+                else:
+                    self.buttonsToCheck[1].handleTouch(x, y, img)
+                    self.buttonsToCheck[1].index = self.time
+                    self.cues += [self.time]
+                self.buttonsToCheck[1].isColliding = True
+
+        if self.buttonsToCheck[2].beingTouched(x, y, img):
+            if not self.buttonsToCheck[2].isColliding:
+                if self.buttonsToCheck[2].on:
+                    self.thread.stop()
+                    self.thread = playback._play_with_simpleaudio(self.song[self.buttonsToCheck[2].index:]
+                                                                  + (50 - self.buttonsToCheck[0].current) / 50 * 30)
+                else:
+                    self.buttonsToCheck[2].handleTouch(x, y, img)
+                    self.buttonsToCheck[2].index = self.time
+                    self.cues += [self.time]
+            self.buttonsToCheck[2].isColliding = True
+
+        if self.buttonsToCheck[3].beingTouched(x, y, img):
+            if not self.buttonsToCheck[3].isColliding:
+                if self.buttonsToCheck[3].on:
+                    self.thread.stop()
+                    self.thread = playback._play_with_simpleaudio(self.song[self.buttonsToCheck[3].index:]
+                                                                  + (50 - self.buttonsToCheck[0].current) / 50 * 30)
+                else:
+                    self.buttonsToCheck[3].handleTouch(x, y, img)
+                    self.buttonsToCheck[3].index = self.time
+                    self.cues += [self.time]
+            self.buttonsToCheck[3].isColliding = True
+
+        if self.buttonsToCheck[4].beingTouched(x, y, img):
+            if not self.buttonsToCheck[4].isColliding:
+                if self.buttonsToCheck[4].on:
+                    self.thread.stop()
+                    self.thread = playback._play_with_simpleaudio(self.song[self.buttonsToCheck[4].index:]
+                                                                  + (50 - self.buttonsToCheck[0].current) / 50 * 30)
+                else:
+                    self.buttonsToCheck[4].handleTouch(x, y, img)
+                    self.buttonsToCheck[4].index = self.time
+                    self.cues += [self.time]
+            self.buttonsToCheck[4].isColliding = True
+
+        if self.buttonsToCheck[5].beingTouched(x, y, img):
+            if len(self.cues) > 0:
+                self.thread.stop()
+                self.thread = playback._play_with_simpleaudio(self.song[self.cues[-1]:]
+                                                              + (50 - self.buttonsToCheck[0].current) / 50 * 30)
+
+        if self.buttonsToCheck[6].beingTouched(x, y, img):
+            if not self.buttonsToCheck[6].isColliding:
+                if self.paused:
+                    self.thread = playback._play_with_simpleaudio(self.song[self.time:]
+                                                                  + (50 - self.buttonsToCheck[0].current) / 50 * 30)
+                    self.paused = False
+                else:
+                    self.paused = True
+                self.buttonsToCheck[-1].handleTouch(x, y, img)
 
 
 class VerticalSlider:
@@ -91,6 +182,7 @@ class VerticalSlider:
         self.current = (min + max) // 2
         self.x = x
         self.y = y
+        self.isColliding = False
 
     def draw(self, img):
         height, width, c = img.shape
@@ -113,6 +205,7 @@ class VerticalSlider:
         height, width, c = img.shape
         yToNum = int((y + height / 4 - self.y) / (height / 2) * self.max)
         self.current = int(min(max(yToNum, self.min), self.max))
+        self.isColliding = True
 
 
 class HorizontalSlider:
@@ -122,6 +215,7 @@ class HorizontalSlider:
         self.current = (min + max) // 2
         self.x = x
         self.y = y
+        self.isColliding = False
 
     def draw(self, img):
         height, width, c = img.shape
@@ -139,10 +233,10 @@ class HorizontalSlider:
         height, width, c = img.shape
         currentToX = int((self.current / self.max) * width / 4) + self.x - width // 8
         return collidesRect((currentToX - 7, self.y - height // 20),
-                            (currentToX + 7, self.y + height // 20),
-                            x, y)
+                            (currentToX + 7, self.y + height // 20), x, y)
 
     def handleTouch(self, x, y, img):
         height, width, c = img.shape
         xToNum = int((x + width // 8 - self.x) / (width // 4) * self.max)
         self.current = int(max(min(xToNum, self.max), self.min))
+        self.isColliding = True
